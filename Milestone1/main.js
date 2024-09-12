9a9c91ec82ad9eabb6d80d72d41665217730575d
warihakhan2 <150653055+warihakhan2@users.noreply.github.com>
2024-09-12 23:24:18 +0500

    Add files via upload

---
+++ Milestone1/main.js
@@ -0,0 +1,13 @@
+// var toggleButton = document.getElementById('btn') as HTMLButtonElement;
+var skills = document.getElementById('skills');
+var display = 0;
+function hideshow() {
+    if (display == 1) {
+        skills.style.display = 'flex';
+        display = 0;
+    }
+    else {
+        skills.style.display = 'none';
+        display = 1;
+    }
+}
